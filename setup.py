9df478e7a94f877b3717c68cdeea5062c30678b4
Eric Busboom <eric@clarinova.com>
2015-02-14 11:37:41 -0800

    .

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Insteon',
+setup(name='esbinsteon',
       version='1.0',
       description='Program to control an insteon interface. ',
       author='Eric Busboom',
       author_email='eric@busboom.org',
       url='http://busboom.org',
       packages=['esbinsteon'],
-      package_dir={'esbinsteon':'src/esbinsteon'},
+      package_dir={'esbinsteon':'esbinsteon'},
       package_data={'esbinsteon': ['config/*']},
       scripts=['scripts/insteon_schedule','scripts/insteon_switch', 'scripts/insteon_install'],
       install_requires=[
